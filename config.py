0184245cbcaeba92a7ca392717f9a6bc4336e36e
Winnie0604 <daniilermolov829@gmail.com>
2025-06-07 00:11:32 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "8143777840:AAHvjt2b5L6zf3RL0TMJ5fbqHEbXltfnsDM"  # токен бота
+CHANNEL_URL = "https://t.me/+v61QTRHyH7I4YzBi"  # Ссылка на канал
+CHANNEL_ID = "-1002384708002"  # id канала. Начинаеться с минуса
+ADMIN_ID = 1248320128
+REF_URL = "https://1wmndv.life/casino/list?open=register&p=ceqc"  # Ссылка на регистрацию
